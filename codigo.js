bc5331d5e0393040631b67b75a4cca645a2dcd6d
disenopc <capitanellipl@gmail.com>
2022-06-02 14:12:00 -0300

    ordene el codigo

--- codigo.js
+++ codigo.js
@@ -1,15 +1,19 @@
 // Determinacion de variables
-let cantidad;
-let precioFinal;
-let resultado;
-let descuentoEfectivo;
 let saludoDos;
+let nombre;
+let precioFinal;
 
 
 //Inicio de la interaccion
 function saludo() {
     saludoDos = prompt("Ingrese su nombre y apellido");
 }
+
+//funcion anonima 
+let mostrar = function(resultado) {
+    alert(resultado);
+};
+
 //Definicion de arrays con objetos
 class Compra {
     constructor(nombre, cantidad) {
@@ -22,22 +26,27 @@ class Producto {
     constructor(nombre, categoria, precio, stock) {
         this.nombre = nombre;
         this.categoria = categoria;
-        this.precio = parseFloat(precio);
+        this.precio = precio;
         this.stock = stock;
 
     }
+
+
 };
+//Definicion de Arrays
+
+
 const productoA = [];
+const compras = [];
 productoA.push(new Producto("waltcan adulto", "Alimento Balanceado", 1200, 0));
 productoA.push(new Producto("waltcan cachorro", "Alimento Balanceado", 1000, 10));
 productoA.push(new Producto("jaspe adulto", "Alimento Balanceado", 1200, 15));
 productoA.push(new Producto("jaspe cachorro", "Alimento Balanceado", 1000, 10));
 console.log(productoA);
-const compras = [];
 
 //Primer paso, proceso de compra, control de stock
 function stock() {
-    let nombre;
+
     do {
         nombre = prompt("Desea agregar un producto a su carrito?Cual?");
         if (nombre == "") {
@@ -65,71 +74,33 @@ function stock() {
 
 };
 
+// function encontrarPrecio() {
+//     if (stockDispo != "") {
+//         productoA[precioF].precio = productoA[precioF].precio * cantidad;
+//         console.log("Ticket de venta. " + " Producto: " + nombre + " Precio: " +
+//             productoA[precioF].precio + " Cantidad" +
+//             cantidad);
+//     }
+//     alert("No se encontro precio para ese producto");
+// }
+
+//Forma de pago
 function formaDePago() {
     precioFinal = prompt('Desea abonar en Efectivo?\n Si \n No \n(Favor de escribir la palabra)');
-}
-
-
-//funcion anonima 
-let mostrar = function(resultado) {
-    alert(resultado);
-};
-
-//funcion flecha
-const precio = (a, b) => {
-    console.log(a * b);
-    return a * b;
-};
-
-
-function operar(num1, num2, precioFinal) {
-    switch (precioFinal) {
-        case 'si':
-            return precio(productoA[3], cantidad);
-        case 'no':
-            return precio(productoA[3], cantidad);
-        default:
-            break;
-    }
-}
-//funcion flecha
-const efectivo = (a, b) => {
-    console.log(a - b);
-    return a - b;
-};
-
-const resta = (a, b) => a - b
-let descuentoEf = 0;
-
-//pago en efectivo
-
-function descuento() {
     if (precioFinal == "si") {
         alert("Se le realizará un descuento por pago en efectivo del 10%")
-        descuentoEf = 0.1 * resultado;
-        descuentoEfectivo = resultado - descuentoEf
     } else {
         alert("Se le cobrará precio de lista");
-        return descuentoEfectivo = resultado;
+
     }
 }
-
-//Se realizaron compras de mercaderia para agregar al array de Producto
-
-productoA.push(new Producto("DogChow Cachorro", "Alimento Balanceado", 1000, 10));
-productoA.unshift(new Producto("DogChow Adulto", "Alimento Balanceado", 1200, 15));
-console.log(productoA);
 
 //Hora de compra
 let ahora = new Date();
 
-
 saludo();
 mostrar("Bienvenido/a " + saludoDos + "! " + "Gracias por visitarnos.");
 stock();
+// encontrarPrecio();
 formaDePago();
-resultado = operar(productoA[3], cantidad, precioFinal);
-mostrar("El precio final de tu producto es $" + resultado);
-descuento();
-mostrar("El precio final de tu producto es $" + descuentoEfectivo);
 alert("Gracias por su compra" + ", " + ahora.toLocaleString());
